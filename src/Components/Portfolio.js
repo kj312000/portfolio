6227d11d6f82069817baedd24d18756339d43c54
Kaustubh Jadhav <kaustubhjadhav36@gmail.com>
2021-10-26 17:46:58 +0530

    nothing

---
+++ src/Components/Portfolio.js
@@ -0,0 +1,43 @@
+import React from "react";
+import './own.css'
+
+const Portfolio = ({ data }) => {
+  if (data) {
+    var projects = data.projects.map(function (projects) {
+      var projectImage = "images/portfolio/" + projects.image;
+      return (
+        <div key={projects.title} className="columns portfolio-item">
+          <div className="item-wrap">
+            <a href={projects.url} title={projects.title}>
+              <img alt={projects.title} src={projectImage} />
+              <div className="overlay">
+                <div className="portfolio-item-meta">
+                  <h5>{projects.title}</h5>
+                  <p>{projects.category}</p>
+                </div>
+              </div>
+              <div className="link-icon">
+                <i className="fa fa-link"></i>
+              </div>
+            </a>
+          </div>
+        </div>
+      );
+    });
+  }
+
+  return (
+    <section>
+      <div className="main_div">
+          <h1 className="h1">Check Out Some of My Works.</h1>
+          <div
+            className="project1"
+          >
+            {projects}
+          </div>
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
---
+++ src/Components/Resume.js
@@ -0,0 +1,117 @@
+import React from "react";
+import LaptopMacIcon from '@mui/icons-material/LaptopMac';
+import SportsSoccerIcon from '@mui/icons-material/SportsSoccer';
+import SportsCricketIcon from '@mui/icons-material/SportsCricket';
+import HikingIcon from '@mui/icons-material/Hiking';
+
+
+const Resume = ({ data }) => {
+  if (data) {
+    var skillmessage = data.skillmessage;
+    var education = data.education.map(function (education) {
+      return (
+        <div key={education.school}>
+          <h3>{education.school}</h3>
+          <p className="info">
+            {education.degree} <span>&bull;</span>
+            <em className="date">{education.graduated}</em>
+          </p>
+          <p>{education.description}</p>
+        </div>
+      );
+    });
+    var work = data.work.map(function (work) {
+      return (
+        <div key={work.company}>
+          <h3>{work.company}</h3>
+          <p className="info">
+            {work.title}
+            <span>&bull;</span> <em className="date">{work.years}</em>
+          </p>
+          <p>{work.description}</p>
+        </div>
+      );
+    });
+    var skills = data.skills.map(function (skills) {
+      var className = "bar-expand " + skills.name.toLowerCase();
+      return (
+        <li key={skills.name}>
+          <span style={{ width: skills.level }} className={className}></span>
+          <em>{skills.name}</em>
+        </li>
+      );
+    });
+  }
+
+  return (
+    <section id="resume">
+      <div className="row education">
+        <div className="three columns header-col">
+          <h1>
+            <span>Education</span>
+          </h1>
+        </div>
+
+        <div className="nine columns main-col">
+          <div className="row item">
+            <div className="twelve columns">{education}</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="row work">
+        <div className="three columns header-col">
+          <h1>
+            <span>Work</span>
+          </h1>
+        </div>
+
+        <div className="nine columns main-col">{work}</div>
+      </div>
+      <div className="row work">
+        <div className="three columns header-col">
+          <h1>
+            <span>Interests</span>
+          <div className="main">
+          <div className="flex">
+                 <SportsCricketIcon className="cric"/>
+                 <h4>Cricket</h4>
+          </div>   
+          <div className="flex">
+          <SportsSoccerIcon className="cric"/>
+                 <h4>Football</h4>
+          </div>
+          <div className="flex">
+          <LaptopMacIcon className="cric"/>
+                 <h4>Coding</h4>
+          </div>
+          <div className="flex">
+          <HikingIcon className="cric"/>
+                 <h4>Trekking</h4>
+          </div>
+          </div>
+            </h1> 
+          
+        </div>
+        </div>
+      
+      <div className="row skill">
+        <div className="three columns header-col">
+          <h1>
+            <span>Skills</span>
+          </h1>
+        </div>
+
+        <div className="nine columns main-col">
+          <p>{skillmessage}</p>
+
+          <div className="bars">
+            <ul className="skills">{skills}</ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Resume;
